c6538067bb5fa2ca221f3cd7b405b5f91218a446
Brad Coffield <bcoffield@gmail.com>
2023-09-20 14:31:19 -0600

    adding Brevo transactional emails

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ import compression from "compression";
 import express from "express";
 const app = express();
 app.use(compression());
-
-import { createClient } from "@sanity/client";
-import cors from "cors"
+import cors from "cors";
 app.use(cors());
+import Brevo from "@getbrevo/brevo";
+var defaultClient = Brevo.ApiClient.instance;
+var apiKey = defaultClient.authentications["api-key"];
+apiKey.apiKey = process.env.BREVO_API_KEY;
+
+/* 
+Sanity.io
+*/
+import { createClient } from "@sanity/client";
 
 export const sanityClient = createClient({
   projectId: "wzuhalz9",
@@ -42,14 +49,77 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
   res.send("Choo Choo! Welcome to your Express app 🚅");
 });
+
 app.post("/interlibrary-loan-request", (req, res) => {
+  var defaultClient = Brevo.ApiClient.instance;
+  var apiKey = defaultClient.authentications["api-key"];
+  apiKey.apiKey = process.env.BREVO_API_KEY;
+
   console.log("POST Contents:", req.body);
-  if (req.body?.userDetails?.email){
-    console.log("Successful /interlibrary-loan-request POST");
-    res.status(200).send({success: "Request successful."}).end()}
-  else {
-    console.log("Failed /interlibrary-loan-request POST");
-    res.status(500).send({ error: "Request failed. Please contact ill@rocky.edu" }).end();}
+  if (req.body?.userDetails?.email) {
+    console.log("SUCCESSFUL /interlibrary-loan-request POST");
+    console.log("Beginning process to send email with Brevo")
+    let apiInstance = new Brevo.TransactionalEmailsApi();
+    let sendSmtpEmail = new Brevo.SendSmtpEmail();
+
+const objectIterator = (object) => {
+  let temp = ""
+  Object.keys(object).forEach((i) => {
+    temp += `<li>${object[i]}</li>`;
+  })
+  return temp
+}
+  
+    let userDetails = req.body.userDetails
+    let requestDetails = req.body.requestDetails
+    let addInfo = req.body.additionalInformation
+    let userDeets = objectIterator(userDetails)
+    let requestDeets = objectIterator(requestDetails)
+   
+
+    sendSmtpEmail.subject = "New Interlibrary Loan Request";
+    sendSmtpEmail.htmlContent = `<html><body><h1>New Interlibrary Loan Request</h1><h2>User Details</h2>
+      <ul>
+        ${userDeets}
+      </ul>
+    <h2>Request Details</h2>
+      <ul>
+        ${requestDeets}
+      </ul>
+    <p></p>Additional Information (if applicable): ${addInfo}</p>
+
+
+    
+    
+    </body></html>`;
+    sendSmtpEmail.sender = { name: "RMC Library Interlibrary Loan", email: "ill@rocky.edu" };
+    sendSmtpEmail.to = [
+      { email: "bradley.coffield@rocky.edu", name: "Brad Coffield" },
+    ];
+    sendSmtpEmail.replyTo = { email: "library@rocky.edu", name: "RMC Library" };
+apiInstance.sendTransacEmail(sendSmtpEmail).then(
+  function (data) {
+    console.log(
+      "API called successfully. Returned data: " + JSON.stringify(data)
+    );  res.status(200).send({ success: "Request successful." }).end();
+  },
+  function (error) {
+    console.log("FAILED /interlibrary-loan-request POST");
+    res
+      .status(500)
+      .send({ error: "Request failed. Please contact ill@rocky.edu" })
+      .end();
+    console.error(error);
+  }
+);
+  
+  } else {
+    console.log("FAILED /interlibrary-loan-request POST");
+    res
+      .status(500)
+      .send({ error: "Request failed. Please contact ill@rocky.edu" })
+      .end();
+  }
 });
 
 const port = process.env.PORT || 3000;
